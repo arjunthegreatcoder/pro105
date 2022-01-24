f0df4b426781a9ef7392aeeb3ef6df7ee88da3d2
arjunthegreatcoder <73948843+arjunthegreatcoder@users.noreply.github.com>
2022-01-24 15:45:14 +0530

    Add files via upload

---
+++ c105/pro105.py
@@ -0,0 +1,28 @@
+import csv
+import math 
+with open ('data.csv',newline = '')as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+data = file_data[0]
+def mean (data):
+    n = len(data)
+    total = 0
+    for x in data:
+        total+=int(x)
+    mean = total/n
+    return mean
+
+squarred_list = []
+for number in data:
+    a = int(number)-mean(data)
+    a = a**2 
+    squarred_list.append(a)
+
+sum = 0
+for i in squarred_list:
+    sum = sum+i
+
+result = sum/(len(data)-1)
+std_dev = math.sqrt(result)
+print(std_dev)
